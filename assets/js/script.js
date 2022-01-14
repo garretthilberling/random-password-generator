c0f853c3d858844fe024a520ee6e9a13e89b3678
garretthilberling <ghilberling@knights.ucf.edu>
2022-01-14 12:45:10 -0500

    began coding initial prompts and if else statement.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,14 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var promptPasswordLength = window.prompt('What is your desired password length? (pick between 8 and 128 characters)');
+
+  // validate prompt answer
+  if (promptPasswordLength === "" || promptPasswordLength === null) {
+    window.alert("Please provide a valid answer. Select number of characters between *8* and *128*. Please try again.");
+    return writePassword();
+  }
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
