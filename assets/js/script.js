7c33f243f9e876107bba6067f53b726abe30f110
garretthilberling <ghilberling@knights.ucf.edu>
2022-01-14 13:21:19 -0500

    used if-else statement to specify criteria for answer. Used isNaN to only include numerical values. Specified range b/w 8 and 128 characters.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,23 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var promptPasswordLength = window.prompt('What is your desired password length? (pick between 8 and 128 characters)');
+  var promptPasswordLength = Number(window.prompt('What is your desired password length? (pick between 8 and 128 characters)'));
 
   // validate prompt answer
-  if (promptPasswordLength === "" || promptPasswordLength === null) {
-    window.alert("Please provide a valid answer. Select number of characters between *8* and *128*. Please try again.");
+  if (promptPasswordLength === "" || promptPasswordLength === null || isNaN(promptPasswordLength)) {
+    window.alert("Please provide a valid numerical answer. Select number of characters between *8* and *128*. Please try again.");
+    return writePassword();
+    console.log("Please try again");
+  } 
+  else if (promptPasswordLength < 8 || promptPasswordLength > 128) {
+    window.alert("Please provide a valid numerical answer. Select number of characters between *8* and *128*. Please try again.");
     return writePassword();
   }
+  else {
+    window.alert("Your password length has been set to " + promptPasswordLength + " characters.");
+  }
+
+  // varPromptCharacterTypes = window.
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
