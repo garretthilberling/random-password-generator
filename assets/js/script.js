cb23ed827ebc311302e8eddfa0a62cd551532bca
garretthilberling <ghilberling@knights.ucf.edu>
2022-01-15 03:10:21 -0500

    continuing to struggle through this, will get help tomorrow

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 // Assignment code here
-
+var passwordLength;
+var passwordString = "";
+// var passwordCharacters
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -8,6 +10,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var promptPasswordLength = Number(window.prompt('What is your desired password length? (pick between 8 and 128 characters)'));
 
+  var passwordCharacters
   // validate prompt answer
   if (promptPasswordLength === "" || promptPasswordLength === null || isNaN(promptPasswordLength) || promptPasswordLength < 8 || promptPasswordLength > 128) {
     window.alert("Please provide a valid numerical answer. Select number of characters between *8* and *128*. Please try again.");
@@ -22,6 +25,7 @@ function writePassword() {
   var promptLowerCase = window.confirm("Include lower case? Select OK for 'yes' or Cancel for 'no.'");
   if (promptLowerCase === true) {
     window.alert("Lower case letters will be included.");
+    passwordCharacters = passwordCharacters + "abcdefghijklmnopqrstuvwxyz";
   } else {
     window.alert("Lower case letters will be excluded.");
   } console.log(promptLowerCase); // OK = true, Cancel = false
@@ -29,6 +33,7 @@ function writePassword() {
   var promptUpperCase = window.confirm("Include upper case? Select OK for 'yes' or Cancel for 'no.'");
   if (promptUpperCase === true) {
     window.alert("Upper case letters will be included.");
+    passwordCharacters = passwordCharacters + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   } else {
     window.alert("Upper case letters will be excluded.");
   } console.log(promptUpperCase); // OK = true, Cancel = false
@@ -36,6 +41,7 @@ function writePassword() {
   var promptNumber = window.confirm("Include numbers? Select OK for 'yes' or Cancel for 'no.'");
   if (promptNumber === true) {
     window.alert("Numbers will be included.");
+    passwordCharacters = passwordCharacters + "0123456789";
   } else {
     window.alert("Numbers will be excluded.");
   } console.log(promptNumber); // OK = true, Cancel = false
@@ -43,16 +49,28 @@ function writePassword() {
   var promptSpecialCharacter = window.confirm("Include special characters? Select OK for 'yes' or Cancel for 'no.'");
   if (promptSpecialCharacter === true) {
     window.alert("Special characters will be included.");
+    passwordCharacters = passwordCharacters + "~!@#$%^&**()_+{}:?<>`";
   } else {
     window.alert("Special characters will be excluded.");
   } console.log(promptSpecialCharacter); // OK = true, Cancel = false
+  
+  var passwordGenerated = passwordCharacters.charAt(Math.floor(Math.random() * promptPasswordLength));
 
-  // window.confirm("Include lower case?", "Include upper case?", "Include numeric?", "Include special characters?");
+  var generatePassword = function(promptPasswordLength, passwordCharacters) {
+    let passwordString = "";
 
-  // var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
+    window.alert("Your password is: " + passwordGenerated);
 
-  // passwordText.value = password;
+    // window.alert(Math.random(passwordCharacters).toString(36).slice(-promptPasswordLength));
+    return passwordString;
+  }
+
+  generatePassword();
+
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
 
 }
 
